2e0da8959a5f9c3fc69dd58008aad4ef02a8785e
Harshal Raverkar <harshal.raverkar@gmail.com>
2021-12-11 15:03:48 +0530

    material and pwa package added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { MaterialModule } from './module/material/material.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+
+    // material module 
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
